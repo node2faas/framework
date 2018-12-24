2b3b1986899dd2c4716026b4acec1d7251664a94
Leonardo Carvalho <leonardocarvalho@Leonardos-MacBook-Pro.local>
2018-12-23 23:59:21 -0200

    Stable version

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
 global.fs = require('fs');
 global.homedir = require('os').homedir();
+global.lockApi =false;
 console.log(global.homedir);
 var funcs = require('./lib/functions');
 var prepare = require('./lib/prepare');
