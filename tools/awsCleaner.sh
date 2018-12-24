f59897bc73500d91e77b038b28bbdd6965dfecfb
Leonardo Carvalho <leonardocarvalho@Leonardos-MacBook-Pro.local>
2018-12-24 01:04:07 -0200

    awsCleaner correction

--- tools/awsCleaner.sh
+++ tools/awsCleaner.sh
@@ -1,8 +1,8 @@
 rm -f api_ids_remove 
 rm -f functions_name_remove 
-lambda list-functions | jq '.Functions[].FunctionName' | sed -e 's/"//g' >> functions_name_remove; done
+aws lambda list-functions | jq '.Functions[].FunctionName' | sed -e 's/"//g' >> functions_name_remove
 for i in ` cat functions_name_remove | sed -e 's/"//g'`; do aws lambda delete-function --function-name $i; done
-aws apigateway get-rest-apis | jq '.items[].id' >> api_ids_remove; done
+aws apigateway get-rest-apis | jq '.items[].id' >> api_ids_remove
 for i in ` cat api_ids_remove | sed -e 's/"//g'`; do aws apigateway delete-rest-api --rest-api-id $i; sleep 61; done
 rm -f api_ids_remove
 rm -f functions_name_remove
