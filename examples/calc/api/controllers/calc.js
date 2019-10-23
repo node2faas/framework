b907c06ee7b9887eeb10b16f3fe00d327248f244
Leonardo Carvalho <leonardo_r_carvalho@yahoo.com>
2019-10-23 07:25:42 -0300

    AWS working properly

--- examples/calc/api/controllers/calc.js
+++ examples/calc/api/controllers/calc.js
@@ -1,6 +1,9 @@
 'use strict';
 
 exports.soma = function(req, res) {
-  var result = req.params.a + req.params.b;
+  var a = 1;
+  var b = 2;
+  var result = a + b;
   res.json(result);
+  return result;
 } ;
